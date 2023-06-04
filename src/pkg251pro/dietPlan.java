4a32b566e8da9575c54d8bb57abddffe6aca371e
SaraBaaqeel <128198564+SaraBaaqeel@users.noreply.github.com>
2023-06-04 03:24:33 +0300

    Add files via upload

---
+++ src/pkg251pro/dietPlan.java
@@ -0,0 +1,25 @@
+
+package pkg251111;
+
+
+public class dietPlan {
+
+    public dietPlan(int plan) {
+        
+    }
+
+        
+public static int Generate_Plan(int calori){
+
+if (calori < 1500 && calori > 900){
+   return 0;  
+  
+}else {
+    
+    return 1;
+}  
+
+}
+
+}
+
