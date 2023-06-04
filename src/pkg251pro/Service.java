b08494c7ebcd266cfbc3883ae7f923b7725345d2
Asma Alamoudi <aabubakar0002@stu.kau.edu.sa>
2023-06-04 03:43:49 +0300

    first commit

---
+++ src/pkg251pro/Service.java
@@ -0,0 +1,47 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pkg251pro;
+//asmaaa
+import java.util.ArrayList;
+
+public class Service {
+    
+   private ArrayList<Workout> workouts;
+
+    public Service() {
+        workouts = new ArrayList<>();
+    }
+
+    public void setWorkouts(ArrayList<Workout> workouts) {
+        this.workouts = workouts;
+    }
+
+    public ArrayList<Workout> getWorkouts() {
+        return workouts;
+    }
+    
+    
+
+    public void addWorkout(Workout workout) {
+        workouts.add(workout);
+    }
+
+    public void removeWorkout(Workout workout) {
+        workouts.remove(workout);
+    }
+
+    public void displayAllWorkouts() {
+        for (Workout workout : workouts) {
+            System.out.println(workout);
+        }
+    }
+
+    public void displayWorkout(Workout workout) {
+        System.out.println(workout);
+    }
+
+    
+}
